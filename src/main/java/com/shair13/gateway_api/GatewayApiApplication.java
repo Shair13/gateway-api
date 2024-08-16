6f633cbb2061e123301edf8f8143401665a68073
Szer13 <shad350@gmail.com>
2024-08-16 11:10:09 +0200

    added swagger and refractorec code

--- src/main/java/com/shair13/gateway_api/GatewayApiApplication.java
+++ src/main/java/com/shair13/gateway_api/GatewayApiApplication.java
@@ -2,17 +2,11 @@ package com.shair13.gateway_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApiApplication.class, args);
-	}
-//
-//	@Bean
-//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//		return builder.routes().build();
-//	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
 }
