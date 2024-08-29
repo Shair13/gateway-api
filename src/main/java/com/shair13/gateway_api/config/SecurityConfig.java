d2e0c2642d2a96055defd2453f3553042132e3e0
Szer13 <shad350@gmail.com>
2024-08-29 17:44:55 +0200

    SecurityConfig update

--- src/main/java/com/shair13/gateway_api/config/SecurityConfig.java
+++ src/main/java/com/shair13/gateway_api/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,17 +16,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().permitAll();
 
-        httpSecurity.oauth2ResourceServer()
-                .jwt();
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
-        httpSecurity
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            authorize.anyRequest().permitAll();
+        });
+
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
     }
